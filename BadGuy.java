11d03a3c6552ac046cb4ea495f8c0e03a7f4b218
ryansutc <ryansutc@gmail.com>
2016-03-07 12:47:33 -0400

    updated Bad guy (beta release)
    Many fixes and revisions, incl. final implementation of items, adding of score calculations, images for bad guys and final screen for game victory.
    
    First Stable beta release version

--- BadGuy.java
+++ BadGuy.java
@@ -1,17 +1,20 @@
 package RPGGame;
 
+import javax.swing.Icon;
+
 public class BadGuy extends Character {
 	private String deathMessage = "You've beaten me.";
 	private boolean weakened = false;
-    
+	private Icon icon; //badguy icon
 	
-	public BadGuy(String name, int maxhealth, int strength, int money){
+	public BadGuy(String name, int maxhealth, int strength, int money, Icon icon){
 		//overridden constructor, set your own parameters
 		this.setMaxhealth(maxhealth);
 		this.setCurhealth(maxhealth);
 		this.setStrength(strength);
 		this.setMoney(money);
 		this.setName(name);
+		this.setIcon(icon);
 	}
 
 	
@@ -42,6 +45,16 @@ public class BadGuy extends Character {
 
 	public void setWeakened(boolean weakened) {
 		this.weakened = weakened;
+	}
+
+
+	public Icon getIcon() {
+		return icon;
+	}
+
+
+	public void setIcon(Icon icon) {
+		this.icon = icon;
 	}
 	
 }
